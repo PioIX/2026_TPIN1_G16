137466ecdb9bb7a9f151e08bf571a93d78a040db
catalinalazzati <catalinalazzati@pioix.edu.ar>
2026-06-17 08:13:05 -0300

    Creado de estructura
    Cree los archivos basicos y las carpetas

---
+++ back/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+const { realizarQuery } = require('./modulos/mysql');
+
+var app = express();
+var port = process.env.PORT || 4000;
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(port, function(){
+    console.log(`Server running in http://localhost:${port}`);
+});
