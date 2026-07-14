fe6ff48373555ec703e7f712e26d05cd721a9de5
rflores-bytes <catalinalazzati@pioix.edu.ar>
2026-07-14 00:18:19 -0300

    arreglando todos los problemas

--- front/scripts/classes/word.js
+++ front/scripts/classes/word.js
@@ -5,14 +5,13 @@ if (!lastIdWord) {
 }
 
 class Word {
-    constructor(word, dificulty, category, admin) {
-        lastIdWord++;
-        this.id = lastIdWord;
-        localStorage.setItem("lastIdWord", lastIdWord);
+    constructor(word, dificulty, category, admin, id) {
+        this.id = id || lastIdWord + 1;
+        localStorage.setItem("lastIdWord", this.id);
         
-        this.word = word;
-        this.dificulty = dificulty;
-        this.category = category;
-        this.admin = admin;
+        this.palabra = word;
+        this.dificultad = dificulty;
+        this.id_categoria = category;
+        this.id_admin = admin;
     }
 }
